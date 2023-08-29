fcb0f092a0d5e11b2aa29f359fab71c9795f8384
erezt.dev <erezt.dev@gmail.com>
2023-08-28 18:11:21 -0700

    Person model + speech method is in

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
+import com.entities.Person
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    val me = Person("Erez")
+    val sayThis = "This is me speaking"
+
+    println("Hello ${me.Name}, can you speak? can you say this: ${me.speak(sayThis)}")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
---
+++ src/main/kotlin/com/entities/Person.kt
@@ -0,0 +1,7 @@
+package com.entities
+
+class Person(var Name:String) {
+    fun speak(textToSpeech: String): String { //would be a com.entities.Voice entity
+        return textToSpeech
+    }
+}
