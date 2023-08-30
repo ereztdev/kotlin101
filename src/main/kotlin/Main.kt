4361222b3f59d33e26fd232ec30d410bbf8af522
erezt.dev <erezt.dev@gmail.com>
2023-08-30 11:03:07 -0700

    Person has a higher order function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,4 +11,12 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    me.displayWithLambda(::printName)
+
 }
+
+fun printName(name: String){
+    println("this is function returned: ${name}")
+}
+
--- src/main/kotlin/com/entities/Person.kt
+++ src/main/kotlin/com/entities/Person.kt
@@ -4,4 +4,8 @@ class Person(var Name:String) {
     fun speak(textToSpeech: String): String { //would be a com.entities.Voice entity
         return textToSpeech
     }
+
+    fun displayWithLambda(func: (s:String) -> Unit){
+        func(Name)
+    }
 }
